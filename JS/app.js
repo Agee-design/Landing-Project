ae571083cf26e81211f95b01a4a3bd1aa18908d8
tigani <tigana.abaker@yahoo.com>
2022-01-02 06:30:00 +0300

    updated my js file

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,6 @@
+ 
+document.addEventListener('DOMContentLoaded', ActiveNavBar, false); 
+
 /**
  * 
  * Manipulating the DOM exercise.
@@ -24,6 +27,8 @@
 */
 
 
+
+
 /**
  * End Global Variables
  * Start Helper Functions
@@ -32,17 +37,46 @@
 /* Start to creat Dynamic NavBarMenu */
 const sections = Array.from(document.getElementsByTagName("section"));
 for(let section of sections){
-    const NavBarMenu = document.quereySelector(".navbar__menu");/* select the element that have id navbar__menu and put in NavBarMenu variabel*/
-    const navBarList = document.getElementById("navbar__list"); /* return the element that has id navbar__list and put in navBarlist variabel*/
-    const listItem = document.createElement('li'); /* creat list item in put in listitem variabel*/
-    const a = document.creatElement("a"); /* creat tag element an assigned into a variael */
-    li.appendChild(a);
+    /* return the element that has id navbar__list and put in navBarlist variabel*/
+    const navBarList = document.getElementById("navbar__list"); 
+    /* creat list item in put in listitem variabel*/
+    const listItem = document.createElement('li');
+    // give the list item the class menu__link to get some styles from the starter code
+    listItem.setAttribute("class", "menu__link")
+    /* creat tag element an assigned into a variael */
+    const a = document.createElement("a"); 
+    // add the text inside your list item anchor from the section data nav
+    a.textContent=section.dataset.nav;
+    listItem.appendChild(a);
     navBarList.appendChild(listItem); /* add the item in last navbar list */
-    NavBarMenu.appendChild(navBarList); /* add the list and put in the last child */
-    const sectionName = sections[i].appendChild(NavBarMenu);
-    document.body.appendChild(sectionName);
-    }
+}
 
+
+
+
+// Make the active section's tab active in the navigation bar 
+function ActiveSection (){
+    window.addEventListener('scroll', function() {
+		const element = document.getElementsByClassName('landing__container');		
+		for (let i = 0; i < element.length; i++) {
+			const position = element[i].getBoundingClientRect();
+			const posit = position.top;
+				if (posit <= window.innerHeight/2){
+					const activeClass = document.getElementsByClassName("your-active-class");
+					activeClass[0].className = activeClass[0].className.replace (" your-active-class", "");
+					element[i].className += " your-active-class";
+			
+					const current = document.getElementsByClassName("active");  
+						if (current.length > 0) {
+							current[0].className = current[0].className.replace(" active", "");
+						}
+			
+					const links = document.getElementById("navbar__list").querySelectorAll('li');
+					links[i].className += " active";
+				};
+		};
+	}); 
+} 
 
 /**
  * End Helper Functions
@@ -54,6 +88,24 @@ for(let section of sections){
 
 
 // Add class 'active' to section when near top of viewport
+function ActiveNavBar (){
+	
+// Get the nav element
+	const linksContainer = document.getElementById("navbar__list");
+	// Get all list with inside the nav
+	const links = linksContainer.querySelectorAll('li');
+	// Loop through the linked list and add the active navBar to the current/clicked button
+	for (let i = 0; i < links.length; i++) {
+  		 links[i].addEventListener("click", function() {
+			const current = document.getElementsByClassName("active");  
+    		if (current.length > 0) {
+      			current[0].className = current[0].className.replace(" active", "");
+    		}
+			// Add the active navBar to the current/clicked button
+    	current[0].className  += "active";
+ 		});
+	};
+}
 
 
 // Scroll to anchor ID using scrollTO event
